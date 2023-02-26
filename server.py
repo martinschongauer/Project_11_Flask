187a83ff816a69b1c5e296928bc3257990e37c3b
martinschongauer <martinschongauer7@gmail.com>
2023-02-26 19:24:49 +0100

    Correcting the code (both Python and HTML) to check the validity of a given email. An empty (from now) test file was created.

--- server.py
+++ server.py
@@ -26,10 +26,34 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/showSummary', methods=['POST'])
+def check_email(email: str) -> dict:
+    """Finds, if it exists, the club associated with a given secretary email
+
+    param email : email used as an ID for a club secretary
+    return : club infos for this email - empty if email is unknown
+    """
+    clubs_found = []
+    for club in clubs:
+        if club['email'] and club['email'] == email:
+            clubs_found.append(club)
+
+    if not clubs_found:
+        return {}
+    else:
+        return clubs_found[0]
+
+
+@app.route('/showSummary',methods=['POST'])
 def show_summary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html', club=club, competitions=competitions)
+    """Log in and render main page
+    """
+    club = check_email(request.form['email'])
+
+    # Detect invalid (=unknown) email -> redirect to index page
+    if not club:
+        return render_template('index.html', email_error=True)
+    else:
+        return render_template('welcome.html', club=club, competitions=competitions)
 
 
 @app.route('/book/<competition>/<club>')
