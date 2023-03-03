f017289b63aebec2224b995eec6d3d29664fc627
martinschongauer <martinschongauer7@gmail.com>
2023-03-03 21:31:12 +0100

    Locust script written.

--- tests/locustfile.py
+++ tests/locustfile.py
@@ -4,8 +4,34 @@ from locust import HttpUser, task
 class ProjectPerfTest(HttpUser):
     @task
     def index(self):
-        response = self.client.get("/printClubs")
+        response = self.client.get("/")
 
-    @task(3)
+    @task
     def board(self):
         response = self.client.get("/printClubs")
+
+    @task
+    def logout(self):
+        response = self.client.get("/logout")
+
+    @task
+    def book(self):
+        response = self.client.get("/book/Spring%20Festival/Simply%20Lift")
+
+    @task
+    def purchase(self):
+        response = self.client.post("/purchasePlaces", data=
+                                    {
+                                        "competition": "Spring Festival",
+                                        "club": "Simply Lift",
+                                        "places": "0"
+                                    }
+                                    )
+
+    @task
+    def summary(self):
+        response = self.client.post("/showSummary", data=
+                                    {
+                                        "email": "john@simplylift.co"
+                                    }
+                                    )
